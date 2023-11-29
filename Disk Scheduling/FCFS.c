d7d318cb5a350c3e184f4ec32ebc37b831855087
Pritam <pritammahata12@gmail.com>
2023-11-29 22:45:28 +0530

    change

--- Disk Scheduling/FCFS.c
+++ Disk Scheduling/FCFS.c
@@ -1,25 +1,24 @@
+// This is wrong  do not write this
+// this is wrong do not write this
+// this is wrong do not write this
 #include <stdio.h>
 #define size 8
 void FCFS(int arr[], int head)
 {
-
     int i;
     int distance, cur_track;
     int seek_count = 0;
     for (i = 0; i < size; i++)
-    
+    {
         cur_track = arr[i];
         distance = cur_track - head;
         seek_count += distance;
         head = cur_track;
-    
-
+    }
     printf("Total number of seek operations = %d", seek_count);
     printf("\n\nSeek Sequence is\n");
     for (i = 0; i < size; i++)
-    {
         printf("%d\n", arr[i]);
-    }
 }
 int main()
 {
